8d1c6e471472a79bdb2a5575cccef9521fcbc660
Churchill <reghoyah@gmail.com>
2025-11-19 18:27:07 +0000

    registered models on to django admin for testing

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, Post, Tag, Comment, Profile, Bookmark, Like
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Comment)
+admin.site.register(Profile)
+admin.site.register(Bookmark)
+admin.site.register(Like)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'status', 'author')
+    search_fields = ['content', 'content']
+    list_filter = ('category', 'status', 'created_at')
+    prepopulated_fields = {'slug': ('title',)}
+    filter_horizontal = ('tags',)
+
+admin.site.register(Post, PostAdmin)
