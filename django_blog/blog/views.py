a11bd6c7ca228dfce2c30be723302b73a5f0b0f1
Churchill <reghoyah@gmail.com>
2025-11-21 01:03:25 +0000

    added the registration view for form rendering

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST) #User submits the form so it can be validated
+        
+        if form.is_valid(): #if form is valid create the user
+            user = form.save()
+            login(request, user) #log the user in
+            return redirect('blog:home') #redirect to the homepage
+        else:
+            form = UserCreationForm() #if a GET request create a blank form
+        return render(request, 'blog/register.gtml' {'form': form}) #render template with form
