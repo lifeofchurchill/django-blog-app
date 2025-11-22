594e88748bb20cdeb8d2a997cee0130511baf685
Churchill <reghoyah@gmail.com>
2025-11-22 21:17:30 +0000

    made correctiosn to login view

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -19,16 +19,16 @@ def register(request):
         form = UserCreationForm() #if a GET request create a blank form
     return render(request, 'blog/register.html', {'form': form}) #render template with form
 
-def login(request):
-    form = AuthenticationForm(request, data = request.POST) #user submits form which is stored in the data variable
-    if form.is_valid:
-        username = form.cleaned_data.get('username')   #after validation cleaned_data cleans the data since django never uses raw data from forms 
-        password = form.cleaned_data.get('password')   #since django cleans it we are extracting the password it cleaned from the data 
-        user = authenticate(username=username, password=password)  #looks at the database to compare the data the user entered
-        if user is not None: #if the users data is in the database
-            login(request, user) #log the user in and creates a session
-            return redirect(home, 'blog:home') # after login redirect them home 
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data = request.POST) #user submits form which is stored in the data variable
+        if form.is_valid():
+            username = form.cleaned_data.get('username')   #after validation cleaned_data cleans the data since django never uses raw data from forms 
+            password = form.cleaned_data.get('password')   #since django cleans it we are extracting the password it cleaned from the data 
+            user = authenticate(username=username, password=password)  #looks at the database to compare the data the user entered
+            if user is not None: #if the users data is in the database
+                login(request, user) #log the user in and creates a session
+                return redirect('blog:home') # after login redirect them home 
     else: 
         form = AuthenticationForm() #blank form 
-        
     return render(request, 'blog/login.html', {'form': form}) #renders this if user isnt logged and has errors and
