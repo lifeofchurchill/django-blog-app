16daa920886d9202512310617f8e2e18042fe4c0
Churchill <reghoyah@gmail.com>
2025-11-26 23:50:19 +0000

    updated url paths for views

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,5 +5,8 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.home, name = 'home'),
-    path('register/', views.register, name = 'register')
+    path('register/', views.register, name = 'register'),
+    path('login/', views.user_login, name = 'login'),
+    path('logout/', views.user_logout, name = 'logout'),
+    path('post/<slug:slug>/', views.post_detail, name = 'post')
 ]
