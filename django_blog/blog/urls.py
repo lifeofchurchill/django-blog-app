0025b8a9e3e303c0f355b770826122f7f530e1d2
Churchill <reghoyah@gmail.com>
2025-12-01 21:08:54 +0000

    url paths updated

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('login/', views.user_login, name = 'login'),
     path('logout/', views.user_logout, name = 'logout'),
     path('post/create/', views.post_create, name = 'post_create'),
-    path('post/<slug:slug>/', views.post_detail, name = 'post')
+    path('post/<slug:slug>/edit/', views.post_edit, name = 'post_edit'),
+    path('post/<slug:slug>/delete/', views.post_delete, name = 'post_delete'),
+    path('post/<slug:slug>/', views.post_detail, name = 'post_detail')
+    
 ]
