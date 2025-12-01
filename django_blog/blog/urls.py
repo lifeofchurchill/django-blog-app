29e02f4a5dd9820eeb21690666df48e95dd51753
Churchill <reghoyah@gmail.com>
2025-12-01 12:41:32 +0000

    updated uurl paths

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('register/', views.register, name = 'register'),
     path('login/', views.user_login, name = 'login'),
     path('logout/', views.user_logout, name = 'logout'),
+    path('post/create/', views.post_create, name = 'post_create'),
     path('post/<slug:slug>/', views.post_detail, name = 'post')
 ]
